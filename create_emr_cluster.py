252a9f427568e8e651daad37d742c7cf77d16aa5
mpenmathsa <mpenmathsa@gmail.com>
2022-05-07 20:37:31 -0700

    test

--- create_emr_cluster.py
+++ create_emr_cluster.py
@@ -13,9 +13,9 @@ def lambda_handler(event, context):
         'Ec2KeyName': 'spark',
         'KeepJobFlowAliveWhenNoSteps': True,
         'TerminationProtected': False,
-        'Ec2SubnetId': 'subnet-b51b2fd3',
-        'EmrManagedMasterSecurityGroup': 'sg-00e712ec1b09f676e',
-        'EmrManagedSlaveSecurityGroup':  'sg-0e9ebbfd18d0669a8'
+        'Ec2SubnetId': 'subnet-0d3560e35068c4188',
+        'EmrManagedMasterSecurityGroup': 'sg-05aedfa35ddc7b126',
+        'EmrManagedSlaveSecurityGroup':  'sg-08df49a57efdeee6f'
     }
 
     configurations = [
@@ -41,7 +41,7 @@ def lambda_handler(event, context):
 
     response = client.run_job_flow(
         Name='PySpark Cluster',
-        LogUri='s3://aws-logs-788660014500-eu-west-1/emr-logs',
+        LogUri='s3://mvp-bigdata/emr-logs',
         ReleaseLabel='emr-5.30.0',
         Instances=instances,
         Configurations=configurations,
